952998e3bfe01f43bff962e5ba1860c47445c38e
alexis.giovoglanian <alexis.giovoglanian@williamhill.com>
2015-09-29 13:50:24 +0100

    deleting djcelery

--- ASM/settings.py
+++ ASM/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import djcelery
-djcelery.setup_loader()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
