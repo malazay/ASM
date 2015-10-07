8e24375be844550c9fdb41beee99b053b299de57
Alexis Giovoglanian <alexis.giovoglanian@bairesdev.com>
2015-10-07 23:34:57 +0100

    Monitor

--- dashboard/views.py
+++ dashboard/views.py
@@ -73,14 +73,12 @@ def ajax(request):
     data = coredata
     return HttpResponse(json.dumps(data), content_type="application/json")
 
-
 def adb_devices_json(request):
     data = []
     for device in adb()[1:]:
         if len(device) > 0:
             data.append(device.split('\t'))
     return HttpResponse(json.dumps(data), content_type="application/json")
-
 
 def adb_devices(request):
     data = []
@@ -90,7 +88,6 @@ def adb_devices(request):
             data.append(device.split('\t'))
     context = {'devices': data}
     return render(request, 'dashboard/adb.html', context)
-
 
 def adb_reboot(request, device_name):
     reboot(device_name)
@@ -102,7 +99,6 @@ def adb_reboot(request, device_name):
             data.append(device.split('\t'))
     context = {'devices': data}
     return render(request, 'dashboard/adb.html', context)
-
 
 def stop_chromedriver(request, server_id):
     server = get_object_or_404(Server, pk=server_id)
